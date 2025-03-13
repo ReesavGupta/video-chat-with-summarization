ed83c609539ecb1005dbaf675ac0fb63c8178f01
ReesvGupta <guptareesav16@gmail.com>
2025-03-13 11:15:44 +0530

    Add room joining and peer retrieval functionality with UUID generation

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,28 +3,83 @@ import { useEffect, useState } from 'react'
 
 export default function Home() {
   const [socket, setSocket] = useState<WebSocket | undefined>(undefined)
+  const [joinedRoom, setJoinedRoom] = useState<boolean>(false)
+  const [roomIdInput, setRoomIdInput] = useState<string>('')
+  const [peerId, setPeerId] = useState<string>('')
 
   const wsUrl = 'http://localhost:3000'
 
   useEffect(() => {
     const socket = new WebSocket(wsUrl)
-    setSocket(socket)
+    if (socket) {
+      setSocket(socket)
+      socket.onmessage = (event: MessageEvent) => {
+        const message = JSON.parse(event.data)
+        console.log(`this is message : `, message)
+        switch (message.type) {
+          case 'joined-room':
+            setPeerId(message.data.peerId)
+            setJoinedRoom(true)
+            console.log(message.data.peerId)
+            break
+
+          case 'all-peers':
+            console.log(`this is message.data: `, message.data)
+            break
+        }
+      }
+    }
   }, [])
 
   function handleJoinRoom() {
-    const roomId = 'asdasdasdas'
-    socket?.send(
-      JSON.stringify({
-        type: 'join-room',
-        data: { roomId },
-      })
-    )
+    console.log(roomIdInput)
+    if (roomIdInput) {
+      socket?.send(
+        JSON.stringify({
+          type: 'join-room',
+          data: { roomId: roomIdInput },
+        })
+      )
+    }
   }
-
+  function handleGetAllPeers() {
+    if (roomIdInput) {
+      socket?.send(
+        JSON.stringify({
+          type: 'get-all-peers',
+          data: {
+            roomId: roomIdInput,
+          },
+        })
+      )
+    }
+  }
   return (
     <div>
-      <div>this is hello world</div>
-      <button onClick={handleJoinRoom}>Join Room</button>
+      <div>this is ajuBhetumKtaHo.io</div>
+      <input
+        type="text"
+        placeholder="room I.D."
+        value={roomIdInput}
+        onChange={(e) => setRoomIdInput(e.target.value)}
+        className="border"
+      />
+      {!joinedRoom ? (
+        <button
+          onClick={handleJoinRoom}
+          className="border mx-1 bg-blue-700 text-white"
+        >
+          Join Room
+        </button>
+      ) : (
+        <></>
+      )}
+      <button
+        onClick={handleGetAllPeers}
+        className="border mx-1 bg-blue-700 text-white"
+      >
+        get all peers
+      </button>
     </div>
   )
 }
--- server/src/ws.ts
+++ server/src/ws.ts
@@ -1,6 +1,7 @@
 import WebSocket, { WebSocketServer } from 'ws'
 import { Room } from './room/Room'
 import type { Router } from 'mediasoup/node/lib/RouterTypes'
+import uuidSingleton from './utils/generateUuid'
 
 export const createConnection = async (
   wss: WebSocketServer,
@@ -9,48 +10,70 @@ export const createConnection = async (
   let rooms: Map<string, Room> = new Map()
 
   wss.on('connection', (socket: WebSocket) => {
-    socket.on('message', async (event) => {
-      const message = JSON.parse(event.toString())
+    socket.on('message', async (event: string) => {
+      const message = JSON.parse(event)
 
       console.log(`this is the message:`, message)
 
       switch (message.type) {
         case 'join-room':
-          await handleJoinRoom(message)
+          await handleJoinRoom(socket, message)
           break
         case 'get-all-peers':
-          ;(async (message: { type: string; data: { roomId: string } }) => {
-            if (message && message.data) {
-              const { roomId } = message.data
-              if (rooms.has(roomId)) {
-                const room = rooms.get(roomId)
-                socket.send(
-                  JSON.stringify({
-                    peers: room?.peers,
-                  })
-                )
-              }
-            }
-          })(message)
+          await getAllPeers(socket, message)
           break
       }
     })
   })
 
-  async function handleJoinRoom(message: {
-    type: string
-    data: { roomId: string }
-  }) {
+  async function handleJoinRoom(
+    socket: WebSocket,
+    message: {
+      type: string
+      data: { roomId: string }
+    }
+  ) {
     const { roomId } = message.data
 
+    const peerId = uuidSingleton.generate()
     if (!rooms.has(roomId)) {
       const room = new Room(roomId, router)
       await room.init()
-      room.addPeer('peerId')
+      // i need to generate a uuid and send it back to the client here
+
+      room.addPeer(peerId)
       rooms.set(roomId, room)
     }
 
     const room = rooms.get(roomId)!
-    room.addPeer('peerId')
+    room.addPeer(peerId)
+    socket.send(
+      JSON.stringify({
+        type: 'joined-room',
+        data: {
+          peerId,
+        },
+      })
+    )
+  }
+
+  async function getAllPeers(
+    socket: WebSocket,
+    message: { type: string; data: { roomId: string } }
+  ) {
+    if (message && message.data) {
+      const { roomId } = message.data
+      console.log(`this is roomId: `, roomId)
+      // console.log(`rooms:`, rooms)
+      if (rooms.has(roomId)) {
+        const room = rooms.get(roomId)
+        socket.send(
+          JSON.stringify({
+            type: 'all-peers',
+            peers: room?.peers,
+          })
+        )
+      }
+    }
   }
 }
